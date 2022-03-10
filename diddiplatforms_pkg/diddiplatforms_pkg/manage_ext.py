8440db53ca0469eaad6c941bb34e62c96e81b974
Diego Ramirez <dr01191115@gmail.com>
2022-03-10 14:17:40 -0600

    Small modification to a warning message

--- diddiplatforms_pkg/diddiplatforms_pkg/manage_ext.py
+++ diddiplatforms_pkg/diddiplatforms_pkg/manage_ext.py
@@ -20,7 +20,7 @@ class UnexpectedFilename(Warning):
 def install_extension(file, no_pyxapp=False):
     dest = os.path.join(find_path.get_user_ubication(), "levels")
     if no_pyxapp:
-        utils.warn("Levels that are not packaged as Pyxel apps are discouraged when not in the standard levels.", DiscouragedMethod)
+        utils.warn("Levels that are not packaged as Pyxel apps are discouraged, since they need Python strictly.", DiscouragedMethod)
         if not file.endswith(".zip"):
             utils.warn(f"Expected zip files, but the file name ('{file}') does not look like one.", UnexpectedFilename)
         dest_file = zipfile.ZipFile(file)
