8ab333811c2fce074bd79587ba622b8ff0957873
Diego Ramirez <dr01191115@gmail.com>
2022-04-11 14:02:35 -0500

    Modify the 'main app' to work as... an app

--- diddiplatforms_pkg/runner.py
+++ diddiplatforms_pkg/runner.py
@@ -12,20 +12,34 @@ from . import utils
 
 
 class PyxelAppRunner:
-    "The class to run the Diddi Platforms app."
+    """
+    The class to run the Diddi Platforms app.
+    It is a basic Pyxel app, that works as the
+    'main operations room'. The levels are run
+    in a separate window, as a unique Pyxel app.
+    """
 
-    def __init__(
+    def __init__(self):
+        self.reset_values()
+        pyxel.init(200, 200, "Diddi Platforms -- Welcome")
+        pyxel.run(self.update, self.draw)
+
+    def reset_values(self):
+        self.levels_path = os.path.join(utils.get_user_ubication(), "levels")
+        os.chdir(self.levels_path)
+
+    def update(self):
+        pass
+
+    def draw(self):
+        pass
+
+    def run_file(
         self,
         app_type="pyxapp",
-        title="Diddi Platforms",
-        init_width=100,
-        init_height=100,
         **kwargs,
     ):
         self.app_type = app_type
-        self.levels_path = os.path.join(utils.get_user_ubication(), "levels")
-
-        os.chdir(self.levels_path)
 
         if self.app_type == "pyxapp":
             if "pyxapp_file" not in kwargs:
@@ -34,21 +48,11 @@ class PyxelAppRunner:
         elif self.app_type == "py":
             if "py_file" not in kwargs:
                 raise ValueError("Keyword argument 'py_file' not found")
-            try:
-                if kwargs["no_pyxel_init"] is False:
-                    self.pyxel_init(init_width, init_height, title)
-            except KeyError:
-                pass
             self.run_py(kwargs["py_file"])
-        elif self.app_type not in ("pyxapp", "py"):
+        else:
             raise ValueError(
                 f"Expected 'app_type' to be one of ('pyxapp', 'py'), got '{self.app_type}'"
             )
-
-    def pyxel_init(self, w, h, t):
-        # NOTE: We set capture_sec to 30
-        # to enable video screenshots :)
-        pyxel.init(w, h, t, capture_sec=30)
 
     def run_pyxapp(self, file):
         """
@@ -61,7 +65,7 @@ class PyxelAppRunner:
         # **should** keep pinned for safety reasons.
         pyxel.cli._play_pyxel_app(file + ".pyxapp")
 
-    def run_py(self, file, configfile):
+    def run_py(self, file):
         """
         Given the Python file, run the app.
         """
